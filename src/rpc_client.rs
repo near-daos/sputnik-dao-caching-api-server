55d83523b915a9796d577f07ab5d3500c56e42ea
Kolya <norfolknorfolks@gmail.com>
2025-05-04 13:03:42 +0200

    Add support for the fastnear api.

--- src/rpc_client.rs
+++ src/rpc_client.rs
@@ -7,10 +7,17 @@ static RPC_CLIENT: OnceLock<Arc<JsonRpcClient>> = OnceLock::new();
 
 /// Returns a shared instance of the RPC client
 pub fn get_rpc_client() -> Arc<JsonRpcClient> {
-    let rpc_url = env::var("NEAR_RPC_URL").unwrap_or("http://127.0.0.1:3030".to_string());
     RPC_CLIENT
         .get_or_init(|| {
-            let client = JsonRpcClient::connect(rpc_url);
+            let rpc_url = env::var("NEAR_RPC_URL").unwrap_or("http://127.0.0.1:3030".to_string());
+            let mut client = JsonRpcClient::connect(rpc_url);
+            if let Some(key) = env::var("NEAR_FAST_API_KEY").ok() {
+                let headers = client.headers_mut();
+                headers.insert(
+                    reqwest::header::AUTHORIZATION,
+                    reqwest::header::HeaderValue::from_str(&key).unwrap(),
+                );
+            }
             Arc::new(client)
         })
         .clone()
