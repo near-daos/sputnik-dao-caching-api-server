0a5d732f1dd4c82f44727ef7d1c8d43914d8e58a
Megha-Dev-19 <100185149+Megha-Dev-19@users.noreply.github.com>
2025-05-26 23:06:42 +0530

    fix failing test

--- src/rpc_client.rs
+++ src/rpc_client.rs
@@ -10,7 +10,8 @@ pub fn get_rpc_client() -> Arc<JsonRpcClient> {
     RPC_CLIENT
         .get_or_init(|| {
             dotenvy::dotenv().ok();
-            let rpc_url = env::var("NEAR_RPC_URL").unwrap_or("http://127.0.0.1:3030".to_string());
+            let rpc_url =
+                env::var("NEAR_RPC_URL").unwrap_or("https://rpc.mainnet.fastnear.com".to_string());
             let mut client = JsonRpcClient::connect(rpc_url);
             if let Some(key) = env::var("NEAR_FAST_API_KEY").ok() {
                 let headers = client.headers_mut();
